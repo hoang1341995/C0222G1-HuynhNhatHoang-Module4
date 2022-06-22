76384a5125d709f35a1caac821e751b1fa411933
hoang1341995 <hoang1341995@gmail.com>
2022-06-22 10:40:52 +0700

    updatye
    updatye

--- ss6_ JPA/exercise/application_blog/src/main/java/example/controller/BlogController.java
+++ ss6_ JPA/exercise/application_blog/src/main/java/example/controller/BlogController.java
@@ -22,49 +22,49 @@ public class BlogController {
         return "index";
     }
 
-//    @GetMapping(value = "/create")
-//    public String showCreate(Model model) {
-//        model.addAttribute("blog", new Blog());
-//        return "create";
-//    }
-//
-//    @PostMapping("/save")
-//    public String saveBlog(Blog blog, RedirectAttributes redirect) {
-//        iBlogService.save(blog);
-//        redirect.addFlashAttribute("mess", "Thêm thành công");
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping(value = "/{id}/edit")
-//    public String editBlog(@PathVariable Integer id, Model model) {
-//        model.addAttribute("blogFindById", iBlogService.findById(id));
-//        return "edit";
-//    }
-//
-//    @PostMapping("/update")
-//    public String updateBlog(Blog blog, RedirectAttributes redirect) {
-//        iBlogService.update(blog);
-//        redirect.addFlashAttribute("mess", "Sửa thành công");
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping(value = "/{id}/view")
-//    public String viewBlog(@PathVariable Integer id, Model model) {
-//        model.addAttribute("blog", iBlogService.findById(id));
-//        return "view";
-//    }
-//
-//    @GetMapping("{id}/delete")
-//    public String deleteBlog(@PathVariable Integer id, RedirectAttributes redirect) {
-//        iBlogService.remove(id);
-//        redirect.addFlashAttribute("mess", "xoá thành công");
-//        return "redirect:/";
-//    }
-//
-//    @GetMapping("/search")
-//    public String searchByName(String name, Model model) {
-//        model.addAttribute("blogList", iBlogService.searchByName(name));
-//        return "index";
-//    }
+    @GetMapping(value = "/create")
+    public String showCreate(Model model) {
+        model.addAttribute("blog", new Blog());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String saveBlog(Blog blog, RedirectAttributes redirect) {
+        iBlogService.save(blog);
+        redirect.addFlashAttribute("mess", "Thêm thành công");
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/{id}/edit")
+    public String editBlog(@PathVariable Integer id, Model model) {
+        model.addAttribute("blogFindById", iBlogService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String updateBlog(Blog blog, RedirectAttributes redirect) {
+        iBlogService.update(blog);
+        redirect.addFlashAttribute("mess", "Sửa thành công");
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/{id}/view")
+    public String viewBlog(@PathVariable Integer id, Model model) {
+        model.addAttribute("blog", iBlogService.findById(id));
+        return "view";
+    }
+
+    @GetMapping("{id}/delete")
+    public String deleteBlog(@PathVariable Integer id, RedirectAttributes redirect) {
+        iBlogService.remove(id);
+        redirect.addFlashAttribute("mess", "xoá thành công");
+        return "redirect:/";
+    }
+
+    @GetMapping("/search")
+    public String searchByName(String name, Model model) {
+        model.addAttribute("blogList", iBlogService.searchByName(name));
+        return "index";
+    }
 
 }
