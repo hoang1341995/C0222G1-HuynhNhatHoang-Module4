76ea237ea5f702c7c9280eeec4138b428413a549
hoang1341995 <hoang1341995@gmail.com>
2022-07-01 00:02:47 +0700

    update
    update

---
+++ ss12_ jQuery_AJAX/exercise/blog_application/src/main/java/com/example/blog_application/controller/BlogController.java
@@ -0,0 +1,97 @@
+package com.example.blog_application.controller;
+
+import com.example.blog_application.model.Blog;
+import com.example.blog_application.service.IBlogService;
+import com.example.blog_application.service.ICategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+public class BlogController {
+
+    @Autowired
+    private IBlogService iBlogService;
+
+    @Autowired
+    private ICategoryService iCategoryService;
+
+    @GetMapping(value = "/")
+    public String showIndex(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+       Sort sort = Sort.by("posting_date").ascending();
+        Page<Blog> list = iBlogService.findAll(PageRequest.of(page, 2, sort));
+        model.addAttribute("blogList",list);
+        return "index";
+    }
+
+    @GetMapping(value = "/create")
+    public String showCreate(Model model) {
+        model.addAttribute("blog", new Blog());
+        model.addAttribute("categoryList", iCategoryService.findAll());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String saveBlog(Blog blog, RedirectAttributes redirect) {
+        iBlogService.save(blog);
+        redirect.addFlashAttribute("mess", "Thêm thành công");
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/{id}/edit")
+    public String editBlog(@PathVariable Integer id, Model model) {
+        model.addAttribute("blogFindById", iBlogService.findById(id));
+        model.addAttribute("categoryList", iCategoryService.findAll());
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String updateBlog(Blog blog, RedirectAttributes redirect) {
+        iBlogService.update(blog);
+        redirect.addFlashAttribute("mess", "Sửa thành công");
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/{id}/view")
+    public String viewBlog(@PathVariable Integer id, Model model) {
+        model.addAttribute("blog", iBlogService.findById(id));
+        return "view";
+    }
+
+    @GetMapping("{id}/delete")
+    public String deleteBlog(@PathVariable Integer id, RedirectAttributes redirect) {
+        iBlogService.remove(id);
+        redirect.addFlashAttribute("mess", "xoá thành công");
+        return "redirect:/";
+    }
+
+    @GetMapping("/search")
+    public String searchByName(@RequestParam(name = "page", defaultValue = "0") int page,String name, Model model) {
+        Sort sort = Sort.by("posting_date").ascending();
+        Page<Blog> blogList = iBlogService.searchByName(name,PageRequest.of(page, 2, sort));
+        model.addAttribute("blogList", blogList);
+        return "index";
+    }
+
+    @GetMapping("/ajaxSearch/{key}")
+    public ResponseEntity<?> ajaxSearch(@PathVariable String key) {
+        System.out.println(key);
+        List<Blog> list = iBlogService.ajaxSearchByName(key);
+        if (!list.isEmpty()){
+            return new ResponseEntity<>(list,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
+}
---
+++ ss12_ jQuery_AJAX/exercise/blog_application/src/main/java/com/example/blog_application/service/BlogService.java
@@ -0,0 +1,71 @@
+package com.example.blog_application.service;
+
+import com.example.blog_application.model.Blog;
+import com.example.blog_application.repository.IBlogRepository;
+import com.example.blog_application.repository.ICategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class BlogService implements IBlogService {
+
+    @Autowired
+    private IBlogRepository iBlogRepository;
+
+    @Autowired
+    private ICategoryRepository iCategoryRepository;
+
+    @Override
+    public Page<Blog> findAll(Pageable pageable) {
+        return iBlogRepository.findAll(pageable);
+    }
+
+    @Override
+    public Page<Blog> findPostByCategory(String category, Pageable pageable) {
+        return iBlogRepository.findPostByCategory(iCategoryRepository.searchIdByName(category),pageable);
+    }
+
+    @Override
+    public void save(Blog blog) {
+        iBlogRepository.save(blog);
+    }
+
+    @Override
+    public Blog findById(int id) {
+        return iBlogRepository.findById(id);
+    }
+
+    @Override
+    public void update(Blog blog) {
+        iBlogRepository.update(blog.getTitle(), blog.getContent(), blog.getDate(), blog.getId());
+    }
+
+    @Override
+    public void remove(Integer id) {
+        iBlogRepository.remove(id);
+    }
+
+    @Override
+    public Page<Blog> searchByName(String title, Pageable pageable) {
+        return iBlogRepository.searchByName("%" + title + "%", pageable);
+    }
+
+    @Override
+    public List<Blog> ajaxSearchByName(String title) {
+        return iBlogRepository.ajaxSearchByName("%" + title + "%");
+    }
+
+    @Override
+    public Page<Blog> findPostById(Integer id, Pageable pageable) {
+        return iBlogRepository.findPostById(id,pageable);
+    }
+
+    public List<Blog> findAll() {
+        return iBlogRepository.findAll();
+    }
+}
